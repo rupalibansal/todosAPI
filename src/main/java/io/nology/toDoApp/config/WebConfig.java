b9a079e901573eb824298261461e5850477cc1da
rupalibansal <r.rupali211@gmail.com>
2024-09-08 11:32:00 +1000

    updated deploy

--- src/main/java/io/nology/toDoApp/config/WebConfig.java
+++ src/main/java/io/nology/toDoApp/config/WebConfig.java
@@ -8,9 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @SuppressWarnings("null")
     public void addCorsMappings(CorsRegistry registry) {
-        String[] allowedOrigins = { "http://localhost:5173/", "http://127.0.0.1:5173/", "http://localhost",
+        String[] allowedOrigins = { "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost",
                 "http://127.0.0.1", "http://localhost:80",
                 "http://ec2-3-24-134-69.ap-southeast-2.compute.amazonaws.com" };
-        registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedMethods("*").allowedHeaders("*");
+        registry
+                .addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
