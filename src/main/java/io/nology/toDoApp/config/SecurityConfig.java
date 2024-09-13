7fdef2c93dc3cfaedaab620f44c1e272395b7c0c
rupalibansal <r.rupali211@gmail.com>
2024-09-13 20:55:31 +1000

    added security config

---
+++ src/main/java/io/nology/toDoApp/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults())
+                .csrf(csrf -> csrf.disable()) // Disable CSRF
+                .authorizeHttpRequests(requests -> requests
+                        .anyRequest().permitAll()); // Allow all requests
+        return http.build();
+    }
+}
