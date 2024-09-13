c41a37d61afcaa50f93f6657d63867796d9e7204
rupalibansal <r.rupali211@gmail.com>
2024-09-13 20:37:06 +1000

    updated conf

--- src/main/java/io/nology/toDoApp/config/WebConfig.java
+++ src/main/java/io/nology/toDoApp/config/WebConfig.java
@@ -9,21 +9,15 @@ public class WebConfig implements WebMvcConfigurer {
     @SuppressWarnings("null")
     public void addCorsMappings(CorsRegistry registry) {
         String[] allowedOrigins = {
-                "http://localhost:5173",
-                "http://127.0.0.1:5173",
                 "http://localhost",
                 "https://todo.bansalz.com",
-                "https://todo.bansalz.com/",
-                "http://todo.bansalz.com",
-                "https://bansalz.com",
-                "https://bansalz.com/",
+                "http://todo.bansalz.com"
         };
         registry
                 .addMapping("/**")
                 .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true)
-                .exposedHeaders("Access-Control-Allow-Origin");
+                .allowCredentials(true);
     }
 }
